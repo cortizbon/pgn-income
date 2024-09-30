435f031b27e07c4c0c31c2257cfebc4ca9a1cefb
Carlos Ortiz <carlosortizbon@gmail.com>
2024-09-30 11:11:07 -0500

    first test

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ st.title("Ingresos del PGN")
 df = pd.read_csv('income.csv')
 
 st.dataframe(df)
+
+fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+
+df.groupby('year')['Valor'].sum().plot(kind='line', ax=ax)
+
+st.pyplot(fig)
