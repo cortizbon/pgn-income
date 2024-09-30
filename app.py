b02dbb5de3ad40f0c571d04e73d167a508016255
Carlos Ortiz <carlosortizbon@gmail.com>
2024-09-30 11:16:23 -0500

    new plot

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ df = pd.read_csv('income.csv')
 
 st.dataframe(df)
 
-fig, ax = plt.subplots(1, 1, figsize=(14, 8))
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
 
 df.groupby('Año')['Valor'].sum().plot(kind='line', ax=ax)
+
+st.pyplot(fig)
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+
+df[df['Sector'] == 'Nación'].groupby('Año')['Valor'].sum().plot(kind='line', ax=ax)
 
 st.pyplot(fig)
